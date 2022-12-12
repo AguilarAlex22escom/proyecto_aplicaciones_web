fde03317d7ed0c1237cb06e54a03a12f8c625c69
Ramírez Aguilar Marc Alexander <mramireza1700@alumno.ipn.mx>
2022-12-11 18:12:48 -0600

    Inicio del proyecto

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// Función para el apartado 'escuela de procedencia'.
+let escuelas = document.getElementById('escuelaDeProcedencia')
+
+// Accedemos a las escuelas de procedencia para utilizarlas.
+/* escuelaDeProcedencia.addEventListener('change', (event) => {
+        let opcionSeleccionada = this.options[escuelaDeProcedencia.selectedIndex]
+        console.log(opcionSeleccionada.value + ': ' + opcionSeleccionada.text)
+}); */
+
+/* let ingresarOtraEscuela = () => {
+    let valorSeleccionado = escuelas.options[escuelas.selectedIndex].text;
+    alert(valorSeleccionado)
+} */
